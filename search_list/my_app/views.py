7ff271c979a69b5a971393f4119e43d93db57ef5
elokwentnie <nszczepanek@op.pl>
2021-08-14 01:30:47 +0200

    some form added

--- search_list/my_app/views.py
+++ search_list/my_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    return render(request, 'my_app/new_search.html')
